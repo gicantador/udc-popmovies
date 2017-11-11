81687aeff6e25c866da99a41d63f4bfdf3c3f28d
Giselle Cantador Nogueira <gigicantador@gmail.com>
2017-11-11 20:58:33 -0200

    configuracao inicial

---
+++ udcPopMovies/app/src/main/java/com/pgcn/udcpopmovies/ShowMovies.java
@@ -0,0 +1,13 @@
+package com.pgcn.udcpopmovies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ShowMovies extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_movies);
+    }
+}
