61909fa49aebfc6fd440325efac43fbb2ccf3bc8
Giselle Cantador Nogueira <gigicantador@gmail.com>
2017-11-15 19:29:32 -0200

    Colocando um texto avisando que um dado retornou vazio da busca.

--- udcPopMovies/app/src/main/java/com/pgcn/udcpopmovies/DetailMovieActivity.java
+++ udcPopMovies/app/src/main/java/com/pgcn/udcpopmovies/DetailMovieActivity.java
@@ -4,14 +4,11 @@ import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.pgcn.udcpopmovies.utils.MovieModel;
 import com.squareup.picasso.Picasso;
-
-import org.apache.commons.lang3.StringUtils;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -27,6 +24,8 @@ public class DetailMovieActivity extends AppCompatActivity {
     private TextView mRating;
     private TextView mReleaseDate;
 
+    private String mTextoNaoFornecido;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,6 +38,8 @@ public class DetailMovieActivity extends AppCompatActivity {
         mReleaseDate = (TextView) findViewById(R.id.tv_release_date);
         mPoster = (ImageView) findViewById(R.id.iv_poster);
 
+        mTextoNaoFornecido = getString(R.string.txt_empty_data);
+
         Intent intentThatStartedThisActivity = getIntent();
 
         if (intentThatStartedThisActivity != null) {
@@ -48,26 +49,51 @@ public class DetailMovieActivity extends AppCompatActivity {
                 if (null != movie) {
 //                    Log.d(TAG, " MOVIE SELECIONADO :" + movie.toString());
 
-                    mOriginalTitle.setText(movie.getOriginalTitle());
-                    mSynopsis.setText(movie.getOverview());
-                    mRating.setText(String.valueOf(movie.getVoteAverage()));
-                    mReleaseDate.setText(formataDataRelease(movie.getDtaReleaseDate()));
+                    mOriginalTitle.setText((String) getCleanField(movie.getOriginalTitle()));
+                    mSynopsis.setText((String) getCleanField(movie.getOverview()));
+                    mRating.setText((String) getCleanField(String.valueOf(movie.getVoteAverage())));
+                    mReleaseDate.setText((String) getCleanField(formataDataRelease(movie.getDtaReleaseDate(), movie.getReleaseDate())));
 
                     String imagePath = movie.getPosterPath();
                     if (imagePath != null && !imagePath.isEmpty()) {
-                        Picasso.with(this).load(imagePath).placeholder(R.drawable.progress_animation).into(mPoster);
+                        Picasso.with(this).load(imagePath).placeholder(R.drawable.placeholder_empty).into(mPoster);
                     }
                 }
             }
         }
     }
 
-    private String formataDataRelease(Date dtaReleaseDate) {
+    /**
+     * Formata a data de release conforme um pattern
+     *
+     * @param dtaReleaseDate data de release em data
+     * @param txtReleaseDate data de release em texto
+     * @return data de release formatada
+     */
+    private String formataDataRelease(Date dtaReleaseDate, String txtReleaseDate) {
 
         if (null != dtaReleaseDate) {
             final DateFormat df = new SimpleDateFormat("EEE, d MMM yyyy");
             return df.format(dtaReleaseDate);
         }
-        return StringUtils.EMPTY;
+        return txtReleaseDate;
+    }
+
+    /**
+     * Verifica se o dado está preenchido e em caso negativo, coloca um texto avisando
+     *
+     * @param obj o dado a ser verificado
+     * @return o dado ou o texto
+     */
+    private Object getCleanField(Object obj) {
+        if (null == obj) {
+            return mTextoNaoFornecido;
+        } else if (String.class == obj.getClass()) {
+            String tst = (String) obj;
+            if (tst.trim().isEmpty()) {
+                return mTextoNaoFornecido;
+            }
+        }
+        return obj;
     }
 }
