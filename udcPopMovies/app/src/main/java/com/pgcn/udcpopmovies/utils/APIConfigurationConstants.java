dbfda6692aa582d2fbb1643066b4b9508a36cfef
Giselle Cantador Nogueira <gigicantador@gmail.com>
2017-11-16 18:12:28 -0200

    escondendo apikey novamente4

--- udcPopMovies/app/src/main/java/com/pgcn/udcpopmovies/utils/APIConfigurationConstants.java
+++ udcPopMovies/app/src/main/java/com/pgcn/udcpopmovies/utils/APIConfigurationConstants.java
@@ -6,5 +6,5 @@ package com.pgcn.udcpopmovies.utils;
 
 public final class APIConfigurationConstants {
 
-    private static final String API_KEY = "<API AQUI>";
+    private static final String API_KEY = "<API dfdfdf AQUI>";
 }
